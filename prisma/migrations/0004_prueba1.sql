9e9864893b0fe5af3141831b0ed13fe63708eb44
monokrome <github@nixvy.ninja>
2025-10-29 18:52:43 +0100

    Ahora ya funcionan bien las migrations

---
+++ prisma/migrations/0004_prueba1.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "prueba" TEXT NOT NULL,
+    "description" TEXT,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("clerkId") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Project" ("createdAt", "description", "id", "isPublic", "title", "userId") SELECT "createdAt", "description", "id", "isPublic", "title", "userId" FROM "Project";
+DROP TABLE "Project";
+ALTER TABLE "new_Project" RENAME TO "Project";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
