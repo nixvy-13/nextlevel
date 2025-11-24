11bb4627151e09fcc39a18572e3f00b10e1aa3a8
monokrome <github@nixvy.ninja>
2025-11-25 00:30:25 +0100

    corregido error que impedia que cargase la imagen de clerk en el perfil por estar mal la configuracion

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", ".prisma/client"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
